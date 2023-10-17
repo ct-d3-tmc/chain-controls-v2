c590d267464a1deeb43184ef4dc83266be52c5ac
Arthi Selvaraj <arthi.selvaraj@dot.ca.gov>
2023-10-17 14:03:59 -0700

    Commit for push messages/email

---
+++ src/app/api/cms/messages/push_message.ts
@@ -0,0 +1,82 @@
+import ActiveITSClient from "@entities/ActiveITSClient";
+import ActiveITSDB from "@entities/ActiveITSDB";
+import EmailSender from "@entities/EmailSender";
+import * as env from "@utils/config.js";
+import { NextResponse, type NextRequest } from "next/server";
+
+const api_url: string | undefined = process.env.ACTIVEITS_API_HOSTNAME;
+const api_port: string | undefined = process.env.ACTIVEITS_API_PORT;
+
+export default async function get_message(req: NextRequest, res: NextResponse) {
+  const dict_response: Record<string, string> = {};
+  try {
+    const active_client: ActiveITSClient = new ActiveITSClient(api_url!, Number(api_port));
+    const db_client = ActiveITSDB.getClient();
+    const emailService: EmailSender = new EmailSender();
+    const store_history: SaveHistory = new SaveHistory();
+    const searchParams = req.nextUrl.searchParams;
+    const goldeneye_username: string | null = searchParams.get("goldeneye_username");
+    const operator_selection: string | null = searchParams.get("operator_inputs");
+    const operator_full_name: string | null = searchParams.get("name");
+    const data = searchParams.get("data");
+    const epage_message = searchParams.get("epage_message");
+    const tm_data = searchParams.get("tmcal_data"); //need to verify data type
+    const h_data = searchParams.get("har_data");
+    await active_client.connect();
+    await db_client.connect();
+    try {
+      const dict_cms_num_to_activeITS_num: Promise<"" | Record<string, string>> | any = db_client.get_cms_id_to_ritms_id_dict();
+      const mas_security_token: string = await active_client.login_to_subsystem(env.DATABUS_SUBSYSTEM, goldeneye_username, db_client);
+      const decoded_cml_queues: string = await active_client.get_queue_msgs(mas_security_token, String(goldeneye_username));
+      const tmcal_messages: Record<string, string> = JSON.parse(tm_data!);
+      const har_messages: Record<string, string> = JSON.parse(h_data!);
+
+      const RECEIVED_DATA: string = data ? String(data) : "";
+      const parsedData = JSON.parse(RECEIVED_DATA);
+      let current_msg = "";
+      for (const key in parsedData) {
+        const cms_id: number = dict_cms_num_to_activeITS_num.get(String(key));
+        if (await db_client.is_cms_out_of_service(key)) {
+          console.log("Skipping CMS because it is out of service" + key);
+          continue;
+        }
+        const list_msgIDs_delete = await active_client.parse_msg_id_list(cms_id, decoded_cml_queues);
+        for (const item of list_msgIDs_delete) {
+          const clear_command = await active_client.generate_remove_command(mas_security_token, cms_id, item, String(goldeneye_username));
+          dict_response["Deleted_Message_From_CMS#" + String(key)] = clear_command;
+        }
+        let new_msg_str: any = parsedData[key];
+        if (new_msg_str === undefined || new_msg_str === "") continue;
+        new_msg_str = active_client.html_to_ntcip_str(new_msg_str);
+        const generated_message = await active_client.generate_apply_command(
+          mas_security_token,
+          cms_id,
+          new_msg_str,
+          String(goldeneye_username)
+        );
+        dict_response["Adding_Message_To_CMS#" + String(key)] = generated_message;
+        if (epage_message) {
+          const email_result = await emailService.sendEmail(epage_message);
+          dict_response["EmailResult"] = email_result;
+          const push_cms_message_response = JSON.stringify(dict_response, null, 4);
+          /*  store_history.create_entry( // working on history
+            RECEIVED_DATA,
+            operator_selection,
+            goldeneye_username!,
+            operator_full_name!,
+            tmcal_messages,
+            har_messages,
+            epage_message
+          );*/
+        }
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    } finally {
+      active_client.close();
+      db_client.close();
+    }
+  } catch (error) {
+    console.error("Unable to push messages", error);
+  }
+}
---
+++ src/entities/EmailSender.ts
@@ -0,0 +1,71 @@
+import nodemailer from "nodemailer";
+
+class EmailSender {
+  private transporter: nodemailer.Transporter;
+  private readonly DISABLED: boolean = false;
+  private readonly ENVIRONMENT: string = "PRODUCTION";
+
+  private readonly SMTP_HOST: any = process.env.SMTP_HOST!;
+  private readonly SMTP_PORT: number = parseInt(process.env.SMTP_PORT!);
+  private readonly TIMEOUT_SECS: number = 30; // Replace with your desired timeout value
+  private readonly DEFAULT_EMAIL: string = process.env.DEFAULT_EMAIL!;
+  private readonly KINGVALE_EMAIL: string = process.env.KINGVALE_EMAIL!;
+  private readonly D3_DISPATCHERS_EMAIL: string = process.env.D3_DISPATCHERS_EMAIL!;
+  private readonly SEND_TO_TEST_LIST: string[] = ["jared.sun@dot.ca.gov"];
+  private readonly BCC_RECIPIENTS_LIST: string[] = ["jared.sun@dot.ca.gov"];
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: this.SMTP_HOST,
+      secure: false,
+      port: this.SMTP_PORT,
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+  }
+
+  async sendEmail(e_msg: string): Promise<string> {
+    if (this.DISABLED) {
+      return "Success";
+    }
+
+    const from_email = process.env.DEFAULT_EMAIL;
+    let to_email_list: string[] = [];
+
+    if (process.env.ENVIRONMENT === "PRODUCTION") {
+      if (e_msg.includes("I-80")) {
+        to_email_list.push(this.KINGVALE_EMAIL);
+      } else if (e_msg.includes("US-50") || e_msg.includes("CA-89")) {
+        to_email_list.push(this.D3_DISPATCHERS_EMAIL);
+      } else {
+        to_email_list.push(this.DEFAULT_EMAIL);
+      }
+    } else {
+      to_email_list.push(...this.SEND_TO_TEST_LIST);
+    }
+    const subject = e_msg;
+    const body_text = e_msg;
+
+    const mailOptions = {
+      from: from_email,
+      subject: subject,
+      text: body_text,
+      to: to_email_list.join(", "),
+      bcc: this.BCC_RECIPIENTS_LIST.join(", "),
+    };
+
+    console.log("Sending email:", subject, "to", to_email_list.join(", "), "BCC", this.BCC_RECIPIENTS_LIST);
+
+    try {
+      const info = await this.transporter.sendMail(mailOptions);
+      console.log("Email sent:", info.response);
+      return "Success";
+    } catch (error) {
+      console.error("Email sending failed:", error);
+      return "";
+    }
+  }
+}
+
+export default EmailSender;
